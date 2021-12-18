bf987e1b3d3f96be7747043261c7b050e66b2c78
ioscann <82466160+ioscann@users.noreply.github.com>
2021-12-18 17:56:49 +0300

    Add files via upload

---
+++ division-without-operator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int num1, num2, count = 0;
+
+	printf("first number ->");
+	scanf("%d", &num1);
+	printf("second number ->");
+	scanf("%d", &num2);
+
+	if (num1 == 0 && num2 == 0)
+	{
+		printf("NaN\n");
+		system("pause");
+		exit(0);
+	}
+
+
+	while (num1 >= num2)
+	{
+		num1 -= num2;
+		count++;
+	}
+
+	end:
+
+	printf("conculusion -> %d", count);
+	printf("\nremaining -> %d", num1);
+
+
+	return 0;
+}
